ca29f63f7ee86f147351e33127a79689c20031bc
aashishbista <aashishbista@localhost>
2026-01-29 17:20:40 -0600

    Completed the reverse method

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -2,22 +2,17 @@ package edu.jsu.mcis.cs310;
 
 public class Main {
 
-    public static void main(String[] args) {
-        
-        Main m = new Main();
-        String message = m.getGreeting();
-        
-        System.out.println(message);
-        System.out.println(m.reverse(message));
-        
-    }
-    
-    public String getGreeting() {
+    public static String getGreeting() {
         return "Hello, World!";
     }
-    
-    public String reverse(String message) {
-        return message;
+
+    public static String reverse(String s) {
+        // Use StringBuilder for now
+        return new StringBuilder(s).reverse().toString();
     }
-    
+
+    public static void main(String[] args) {
+        System.out.println(getGreeting());
+        System.out.println(reverse(getGreeting()));
+    }
 }
